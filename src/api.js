69930d12bdc57599f4afd75b3a8f4b2df5e8d0a9
Balaji-Sathesh <balajisathesh.bs@gmail.com>
2022-07-27 16:12:26 +0530

    Removing API Key

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 export const geoApiOptions = {
     method: 'GET',
     headers: {
-        'X-RapidAPI-Key': 'ecd739fa7emshfe19cf0825825c5p191320jsnd6516b275573',
+        'X-RapidAPI-Key': 'ecd7',/*Provide API KEY*/
         'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
     }
 };
@@ -9,5 +9,5 @@ export const geoApiOptions = {
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 export const WEATHER_API_URI = "https://api.openweathermap.org/data/2.5";
-export const WEATHER_API_KEY = "1e36de30c2c2523d0ddbc740d346e1c9";
+export const WEATHER_API_KEY = "1e36";//Provide API KEY
 
