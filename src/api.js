25ae1cb5c30d5703e995b5165f66d5e46f53a8cd
Balaji-Sathesh <balajisathesh.bs@gmail.com>
2022-07-27 16:09:47 +0530

    Implementing Functions

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'ecd739fa7emshfe19cf0825825c5p191320jsnd6516b275573',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URI = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "1e36de30c2c2523d0ddbc740d346e1c9";
+
